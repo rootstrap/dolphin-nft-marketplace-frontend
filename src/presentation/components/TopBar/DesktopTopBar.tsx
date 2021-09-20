30c3c22427bf9207337272b666d66ffd5a38c6b3
Alejoboga20 <alejoboga20@gmail.com>
2021-09-20 17:13:22 -0300

    Removing important from scss files and adding navbar items to locales

--- src/presentation/components/TopBar/DesktopTopBar.tsx
+++ src/presentation/components/TopBar/DesktopTopBar.tsx
@@ -1,35 +1,34 @@
 import { Button, Grid, Typography } from '@material-ui/core';
 import { ReactComponent as DolphinLogo } from '../../../app/assets/Dolphin_Ball.svg';
+import useTranslation from '../../../app/hooks/useTranslation';
 import styles from './TopBar.module.scss';
 
 export const DesktopTopBar = () => {
+  const t = useTranslation();
+
   return (
     <Grid container spacing={2}>
       <Grid item md={2} lg={2}>
         <DolphinLogo className={styles.logo} />
       </Grid>
       <Grid item md={1} lg={1} className={styles.topBar__item}>
-        <Typography>Drops</Typography>
+        <Typography>{t('navBar.drops')}</Typography>
       </Grid>
       <Grid item md={2} lg={1} className={styles.topBar__item}>
-        <Typography>Marketplace</Typography>
+        <Typography>{t('navBar.marketplace')}</Typography>
       </Grid>
       <Grid item md={2} lg={1} className={styles.topBar__item}>
-        <Typography>Promotions</Typography>
+        <Typography>{t('navBar.promotions')}</Typography>
       </Grid>
       <Grid item md={1} lg={5} className={styles.topBar__item}>
-        <Typography>FAQ</Typography>
+        <Typography>{t('navBar.faq')}</Typography>
       </Grid>
 
       <Grid item md={2} lg={1} className={styles.topBar__item}>
-        <Button className={styles.topBar__button} variant="outlined">
-          Sign in
-        </Button>
+        <Button>{t('global.login')}</Button>
       </Grid>
       <Grid item md={2} lg={1} className={styles.topBar__item}>
-        <Button className={styles.topBar__button} variant="outlined">
-          Sign up
-        </Button>
+        <Button>{t('global.signup')}</Button>
       </Grid>
     </Grid>
   );

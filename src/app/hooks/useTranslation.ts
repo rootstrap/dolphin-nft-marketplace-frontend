2892356404337b17bd63acbb47fc2580eb664c11
Alejo Boga <alejoboga20@gmail.com>
2021-09-20 11:06:54 -0300

    Configuring useTranslation hook

---
+++ src/app/hooks/useTranslation.ts
@@ -0,0 +1,10 @@
+import { PrimitiveType, FormatXMLElementFn } from 'intl-messageformat';
+import { useIntl } from 'react-intl';
+
+const useTranslation = () => {
+	const intl = useIntl();
+
+	return (message: string) => intl.formatMessage({ id: message });
+};
+
+export default useTranslation;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { store } from './app/store/store';
 import { App } from './app/App';
+import { store } from './app/store/store';
+import { IntlProvider } from 'react-intl';
+import { Provider } from 'react-redux';
+import { DEFAULT_LANGUAGE } from './app/constants/contants';
+import flatten from 'flat';
+import locales from './app/locales';
 import './app/styles/styles.scss';
+
+const messages = locales['en'];
 
 ReactDOM.render(
 	<React.StrictMode>
-		<Provider store={store}>
-			<App />
-		</Provider>
+		<IntlProvider
+			messages={flatten(messages)}
+			locale={'en'}
+			defaultLocale={DEFAULT_LANGUAGE}
+		>
+			<Provider store={store}>
+				<App />
+			</Provider>
+		</IntlProvider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );

78c3cbc3270cb2b41d85c4e0923b6e9f073e645f
Alejoboga20 <alejoboga20@gmail.com>
2021-09-28 16:56:19 -0300

    Adding constants

--- src/app/constants/contants.ts
+++ src/app/constants/contants.ts
@@ -1,2 +1,14 @@
 export const SUPPORTED_LANGUAGES: string[] = ['en'];
 export const DEFAULT_LANGUAGE: string = 'en';
+
+export const colors = {
+  primary: '#35aed7',
+  error: '#a52a2a',
+  lightGrey: '#eaeaea',
+  midGrey: '#646464',
+  darkGrey: '#5c5c5c',
+  backgroundDark: '#1f1f1f',
+  backgroundPromotion: '#161616',
+  orange: '#f58550',
+  black: '#000',
+};

34f8751a9b885ac3f3427a8829b49b8071d039f0
Alejoboga20 <alejoboga20@gmail.com>
2021-10-05 15:23:35 -0300

    Adding page to routes

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -1,5 +1,7 @@
+import NFTPage from 'presentation/pages/NFT/NFTPage';
 import HomePage from '../../presentation/pages/Home/HomePage';
 import ProfilePage from '../../presentation/pages/Profile/ProfilePage';
+import VerticalPage from '../../presentation/pages/Verticals/VerticalPage';
 import routesPaths from '../constants/routesPath';
 import { Route } from '../interfaces/common/Route';
 
@@ -7,6 +9,18 @@ export const routes: Route[] = [
   {
     path: routesPaths.index,
     component: <HomePage />,
+    exact: true,
+    private: false,
+  },
+  {
+    path: routesPaths.verticals,
+    component: <VerticalPage />,
+    exact: true,
+    private: false,
+  },
+  {
+    path: routesPaths.nftDetails,
+    component: <NFTPage />,
     exact: true,
     private: false,
   },

a196ced95effd62b289c2a075c4905573378d491
Alejoboga20 <alejoboga20@gmail.com>
2021-10-06 14:52:09 -0300

    Adding NFT Page

--- src/presentation/pages/NFT/Main/Main.tsx
+++ src/presentation/pages/NFT/Main/Main.tsx
@@ -1,5 +1,41 @@
-import { Grid } from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
+import { NFT } from 'app/interfaces/NFT/NFT';
+import { CustomLoader } from 'infrastructure/components/CustomLoader/CustomLoader';
+import { useGetNftDetailsMutation } from 'infrastructure/services/nft/NftService';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import { Item } from './Item';
+import styles from './Main.module.scss';
 
 export const Main = () => {
-  return <Grid container>{/* TODO: Implement details of each nft based on nftId */}</Grid>;
+  const { nftId } = useParams<{ nftId: string }>();
+  const [nft, setNft] = useState<NFT>();
+  const [getNftDetails, { isLoading, isError, isSuccess }] = useGetNftDetailsMutation();
+
+  const loadData = async () => {
+    const data: any = await getNftDetails(nftId);
+    console.log(data.data);
+    setNft(data.data);
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  return (
+    <div className={styles.mainContent}>
+      {isLoading ? (
+        <CustomLoader />
+      ) : (
+        <Grid container className={styles.mainContent__container}>
+          <Grid item className={styles.mainContent__item} xs={12} lg={6}>
+            <iframe className={styles.mainContent__itemVideo} src={nft?.videoUrl} title={nft?.name} />
+          </Grid>
+          <Grid item className={styles.mainContent__item} xs={12} lg={6}>
+            <Item nft={nft} styles={styles} />
+          </Grid>
+        </Grid>
+      )}
+    </div>
+  );
 };

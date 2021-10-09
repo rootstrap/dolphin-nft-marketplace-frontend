d18811cb73f327935bc82859e8ce1f38f0657c70
Alejoboga20 <alejoboga20@gmail.com>
2021-10-09 11:48:34 -0300

    Adding bearer token to requests

--- src/infrastructure/services/Api.ts
+++ src/infrastructure/services/Api.ts
@@ -1,8 +1,21 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { RootState } from 'app/store/store';
 
 // initialize an empty api service that we'll inject endpoints into later as needed
 export const api = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_API_URL }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: process.env.REACT_APP_API_URL,
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).user.token;
+
+      // If we have a token set in state, let's assume that we should be passing it.
+      if (token) {
+        headers.set('authorization', `Bearer ${token}`);
+      }
+
+      return headers;
+    },
+  }),
   endpoints: () => ({}),
 });

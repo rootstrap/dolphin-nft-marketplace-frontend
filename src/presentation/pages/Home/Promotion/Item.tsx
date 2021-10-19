dbb56e5a70b92b99b8d428a5f44f8e4b0b8cc3d6
Alejoboga20 <alejoboga20@gmail.com>
2021-10-19 10:15:01 -0300

    Making carousel with less text

--- src/presentation/pages/Home/Promotion/Item.tsx
+++ src/presentation/pages/Home/Promotion/Item.tsx
@@ -2,28 +2,43 @@ import { Typography } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import New from 'app/assets/New.png';
 import useTranslation from 'app/hooks/useTranslation';
+import { useState } from 'react';
 
 export const Item = ({ id, name, totalQuantity, image, verticalId, styles }: ItemProps) => {
   const t = useTranslation();
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+  const handleMouseEnter = () => setIsVisible(true);
+  const handleMouseLeave = () => setIsVisible(false);
 
   return (
-    <div className={styles.promotionContent__itemContent}>
-      <Link to={`${verticalId}/${id}`}>
-        <div className={styles.promotionContent__imgContainer}>
-          <img src={New} alt="New" className={styles.promotionContent__imgContainerNew} />
-          <img src={image} alt="Promotion" className={styles.promotionContent__imgContainerImg} />
-        </div>
-      </Link>
-      <Typography className={styles.promotionContent__itemTitle} variant="h5" component="div">
-        {name}
-      </Typography>
+    <Link to={`${verticalId}/${id}`}>
+      <div
+        className={styles.promotionContent__itemContent}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <img src={New} alt="New" className={styles.promotionContent__itemContentNew} />
+        <img src={image} alt="Promotion" className={styles.promotionContent__itemContentImg} />
 
-      <div className={styles.promotionContent__favContainer}>
-        <Typography>
-          {totalQuantity ? `${t('verticals.item.totalEdition')} ${totalQuantity}` : 'Single Item'}
-        </Typography>
+        {isVisible && (
+          <div className={styles.promotionContent__itemContentHidden}>
+            <Typography
+              className={styles.promotionContent__itemContentHiddenTitle}
+              variant="h5"
+              component="div"
+            >
+              {name}
+            </Typography>
+
+            <div className={styles.promotionContent__favContainer}>
+              <Typography>
+                {totalQuantity ? `${t('verticals.item.totalEdition')} ${totalQuantity}` : 'Single Item'}
+              </Typography>
+            </div>
+          </div>
+        )}
       </div>
-    </div>
+    </Link>
   );
 };
 

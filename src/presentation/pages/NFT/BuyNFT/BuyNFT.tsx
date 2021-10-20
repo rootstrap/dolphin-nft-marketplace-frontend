366bbb2a76ba4ac3eba2a0456530e426aa97a553
Alejoboga20 <alejoboga20@gmail.com>
2021-10-20 15:51:04 -0300

    Adding locales

--- src/presentation/pages/NFT/BuyNFT/BuyNFT.tsx
+++ src/presentation/pages/NFT/BuyNFT/BuyNFT.tsx
@@ -1,11 +1,13 @@
+import { useContext } from 'react';
 import { Button, Grid, Typography } from '@material-ui/core';
 import { ModalContext } from 'app/context/ModalContext';
 import { useAppSelector } from 'app/hooks/reduxHooks';
 import { NFT } from 'app/interfaces/NFT/NFT';
-import { useContext } from 'react';
+import useTranslation from 'app/hooks/useTranslation';
 import buyStyles from './Buy.module.scss';
 
 export const BuyNFT = ({ nft, handleShowDescription }: BuyNFTProps) => {
+  const t = useTranslation();
   const { setKycModalIsOpen } = useContext(ModalContext);
   const { user } = useAppSelector(state => state.user);
 
@@ -26,17 +28,17 @@ export const BuyNFT = ({ nft, handleShowDescription }: BuyNFTProps) => {
       <Grid item xs={12} lg={5}>
         <div className={buyStyles.buyContent__itemFund}>
           <div className={buyStyles.buyContent__walletContainer}>
-            <Typography component="div">Please activate your wallet to perform your fist purchase</Typography>
+            <Typography component="div">{t('nft.buyNft.activateWallet')}</Typography>
             {!user.kyc1ed && (
               <Button variant="text" onClick={() => setKycModalIsOpen(true)}>
-                Activate wallet{' '}
+                {t('nft.buyNft.activateButton')}
               </Button>
             )}
           </div>
           <div className={buyStyles.buyContent__buttonContainer}>
             <div className={buyStyles.buyContent__priceContainer}>
               <Typography variant="h5" className={buyStyles.buyContent__priceContainer__label}>
-                Price:
+                {t('nft.buyNft.price')}
               </Typography>
               <Typography variant="h4" className={buyStyles.buyContent__priceContainer__price}>
                 {nft?.quoteCurrency} ${nft?.offerPrice}
@@ -44,10 +46,10 @@ export const BuyNFT = ({ nft, handleShowDescription }: BuyNFTProps) => {
             </div>
 
             <Button fullWidth className={buyStyles.buyContent__btn}>
-              Activate your wallet to continue
+              {t('nft.buyNft.continueButton')}
             </Button>
             <Button fullWidth size="large" onClick={handleShowDescription}>
-              Cancel
+              {t('nft.buyNft.cancelButton')}
             </Button>
           </div>
         </div>

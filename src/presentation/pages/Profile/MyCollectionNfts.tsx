40d179f30dda7d9ef24a985423b1e708ae99ca4a
Alejoboga20 <alejoboga20@gmail.com>
2021-10-20 12:14:18 -0300

    Showing empty list in profile as we do not have nfts yet

--- src/presentation/pages/Profile/MyCollectionNfts.tsx
+++ src/presentation/pages/Profile/MyCollectionNfts.tsx
@@ -1,40 +1,29 @@
 import { Grid, Typography } from '@material-ui/core';
-import { Item } from './Item';
-import { useMyCollection } from './useMyCollection';
-import { NFT } from 'app/interfaces/NFT/NFT';
 import useTranslation from 'app/hooks/useTranslation';
 import styles from './MyCollection.module.scss';
 
 export const MyCollectionNfts = () => {
-  const { nfts, isLoading } = useMyCollection();
   const t = useTranslation();
 
   return (
     <Grid container className={styles.myCollectionContent}>
-      <Grid item xs={1}></Grid>
-      <Grid item xs={10}>
-        <Typography variant="h4">{t('profile.collectionTitle')}</Typography>
-      </Grid>
-      <Grid item xs={1}></Grid>
-      <Grid item xs={1}></Grid>
-      <Grid item xs={10}>
-        <Grid container spacing={4}>
-          {nfts.map((nft: NFT) => (
-            <Grid item xs={12} md={6} lg={4} key={nft.id}>
-              <Item
-                styles={styles}
-                id={nft.id}
-                name={nft.name}
-                totalQuantity={nft.totalQuantity}
-                image={nft.imageUrl}
-                ftxId={nft.ftx_id}
-                verticalId={'verticals/sports'}
-              />
-            </Grid>
-          ))}
+      <Grid container className={styles.myCollectionContent__title}>
+        <Grid item xs={1}></Grid>
+        <Grid item xs={10}>
+          <Typography variant="h4">{t('profile.collectionTitle')}</Typography>
         </Grid>
+        <Grid item xs={1}></Grid>
       </Grid>
-      <Grid item xs={1}></Grid>
+
+      <Grid container className={styles.myCollectionContent__items}>
+        <Grid item xs={2}></Grid>
+        <Grid item xs={8}>
+          <Typography variant="h4" gutterBottom className={styles.myCollectionContent__itemsMsg}>
+            You don’t have any collectibles in your gallery yet
+          </Typography>
+        </Grid>
+        <Grid item xs={2}></Grid>
+      </Grid>
     </Grid>
   );
 };

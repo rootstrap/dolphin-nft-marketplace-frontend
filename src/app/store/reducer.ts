63f01710debc8f7b1a48b9a7dd60b58c75ac8243
Alejoboga20 <alejoboga20@gmail.com>
2021-10-22 14:56:22 -0300

    Adding nft slice

--- src/app/store/reducer.ts
+++ src/app/store/reducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { api } from 'infrastructure/services/Api';
 import userReducer from './features/userSlice';
+import nftReducer from './features/nftSlice';
 
 export const rootReducer = combineReducers({
   [api.reducerPath]: api.reducer,
   user: userReducer,
+  nft: nftReducer,
 });

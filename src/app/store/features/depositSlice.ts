0741c7fa487e297943abf7c8a2d456e1adc79072
Alejoboga20 <alejoboga20@gmail.com>
2021-11-10 15:38:38 -0300

    Adding slice for deposit and balances

---
+++ src/app/store/features/depositSlice.ts
@@ -0,0 +1,27 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { Balance, getBalanceFulfiled } from 'infrastructure/services/deposit/DepositService';
+
+const initialState: DepositState = {
+  currentBalance: 0,
+  balances: [],
+};
+
+const depositSlice = createSlice({
+  name: 'deposit',
+  initialState,
+  reducers: {},
+  extraReducers: builder => {
+    builder.addMatcher(getBalanceFulfiled, (state, { payload }) => {
+      console.log('balance payload: ', payload);
+      state.balances = [...payload];
+      state.currentBalance = payload[0].total;
+    });
+  },
+});
+
+interface DepositState {
+  currentBalance: number;
+  balances: Balance[];
+}
+
+export default depositSlice.reducer;

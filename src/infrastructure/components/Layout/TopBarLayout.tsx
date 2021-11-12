e0ab5362919337f14b1306c59b73f81cd66bc502
Alejoboga20 <alejoboga20@gmail.com>
2021-11-12 10:36:40 -0300

    Creating prop to disable categories in topbar

--- src/infrastructure/components/Layout/TopBarLayout.tsx
+++ src/infrastructure/components/Layout/TopBarLayout.tsx
@@ -1,20 +1,21 @@
+import { ReactElement } from 'react';
 import { BottomBar } from 'presentation/components/BottomBar/BottomBar';
 import { CreditCardModal } from 'presentation/components/CreditCardModal/CreditCardModal';
-import { ReactElement } from 'react';
-import { Login } from '../../../presentation/components/Login/Login';
-import { Signup } from '../../../presentation/components/Signup/Signup';
-import { TopBar } from '../../../presentation/components/TopBar/TopBar';
+import { Login } from 'presentation/components/Login/Login';
+import { Signup } from 'presentation/components/Signup/Signup';
+import { TopBar } from 'presentation/components/TopBar/TopBar';
 import { BackgroundLayout } from './BackgroundLayout';
 
 interface TopBarLayoutProps {
   pageComponent: ReactElement;
+  isTopBarDisabled?: boolean;
 }
 
-export const TopBarLayout = ({ pageComponent }: TopBarLayoutProps) => {
+export const TopBarLayout = ({ pageComponent, isTopBarDisabled = false }: TopBarLayoutProps) => {
   return (
     <>
       <BackgroundLayout />
-      <TopBar />
+      <TopBar isTopBarDisabled={isTopBarDisabled} />
       {pageComponent}
       <Login />
       <Signup />

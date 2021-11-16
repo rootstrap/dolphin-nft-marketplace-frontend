329febd9d80cc49ba0a7238b13643e681669de5d
Alejoboga20 <alejoboga20@gmail.com>
2021-11-16 11:11:59 -0300

    Creating routes for creatures

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import NFTPage from 'presentation/pages/NFT/NFTPage';
 import HomePage from 'presentation/pages/Home/HomePage';
 import MyCollection from 'presentation/pages/Profile/MyCollection';
@@ -6,9 +7,62 @@ import CreaturesPage from 'presentation/pages/Categories/Creatures/CreaturesPage
 import VerticalPage from 'presentation/pages/Verticals/VerticalPage';
 import NFTByIdPage from 'presentation/pages/NFTByFtxId/NFTByIdPage';
 import routesPaths from '../constants/routesPath';
-import { Route } from '../interfaces/common/Route';
 
-export const routes: Route[] = [
+export const routes = {
+  isCreatures: [
+    {
+      path: routesPaths.index,
+      component: <CreaturesPage />,
+      exact: true,
+      private: false,
+    },
+  ],
+  isNormalUser: [
+    {
+      path: routesPaths.index,
+      component: <HomePage />,
+      exact: true,
+      private: false,
+    },
+    {
+      path: routesPaths.verticals,
+      component: <VerticalPage />,
+      exact: true,
+      private: false,
+    },
+    {
+      path: routesPaths.creatures,
+      component: <CreaturesPage />,
+      exact: true,
+      private: false,
+    },
+    {
+      path: routesPaths.nftDetails,
+      component: <NFTPage />,
+      exact: true,
+      private: false,
+    },
+    {
+      path: routesPaths.nftById,
+      component: <NFTByIdPage />,
+      exact: true,
+      private: false,
+    },
+    {
+      path: routesPaths.profile,
+      component: <ProfilePage />,
+      private: true,
+    },
+    {
+      path: routesPaths.my_collection,
+      component: <MyCollection />,
+      private: true,
+    },
+  ],
+};
+/* 
+
+[
   {
     path: routesPaths.index,
     component: <HomePage />,
@@ -50,3 +104,6 @@ export const routes: Route[] = [
     private: true,
   },
 ];
+
+
+*/

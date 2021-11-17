c5d0b83300fe95cf6c572cb071af8bbbe679a010
Alejoboga20 <alejoboga20@gmail.com>
2021-11-17 16:20:43 -0300

    Adding locales

--- src/presentation/pages/Profile/Checkboxes/Checkboxes.tsx
+++ src/presentation/pages/Profile/Checkboxes/Checkboxes.tsx
@@ -2,9 +2,11 @@ import { Checkbox, Typography } from '@material-ui/core';
 import { useAppSelector } from 'app/hooks/reduxHooks';
 import { useContext } from 'react';
 import { ModalContext } from '../../../../app/context/ModalContext';
+import useTranslation from 'app/hooks/useTranslation';
 import styles from './Checkboxes.module.scss';
 
 export const Checkboxes = () => {
+  const t = useTranslation();
   const { kyc1ed } = useAppSelector(state => state.user.user);
   const { defaultCreditCard } = useAppSelector(state => state.creditCard);
   const { setKycModalIsOpen, setCcModalIsOpen } = useContext(ModalContext);
@@ -20,7 +22,7 @@ export const Checkboxes = () => {
             <div className={styles.checkboxes__container}>
               <Checkbox checked />
               <Typography variant="h6" className={styles.checkboxes__containerText}>
-                Create Account
+                {t('profile.checkboxes.firstCheck')}
               </Typography>
             </div>
 
@@ -31,7 +33,7 @@ export const Checkboxes = () => {
                 onClick={() => setKycModalIsOpen(!kyc1ed)}
                 className={styles.checkboxes__containerText}
               >
-                Tell us about yourself
+                {t('profile.checkboxes.secondCheck')}
               </Typography>
             </div>
 
@@ -43,16 +45,13 @@ export const Checkboxes = () => {
                   onClick={() => setCcModalIsOpen(!isWalletReady)}
                   className={styles.checkboxes__containerText}
                 >
-                  Activate your wallet
+                  {t('profile.checkboxes.thirdCheck')}
                 </Typography>
               </div>
             )}
           </div>
 
-          <Typography variant="h6">
-            You’re almost ready! You must activate your wallet before you start collecting. We recommend doing
-            this ahead of time -- don’t worry, you won’t be charged until you decide to purchase.
-          </Typography>
+          <Typography variant="h6">{t('profile.checkboxes.description')}</Typography>
         </>
       )}
     </>

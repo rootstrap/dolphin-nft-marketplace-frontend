1d84488489260381beca5f9af26c164999aebe3f
Alejoboga20 <alejoboga20@gmail.com>
2021-11-19 15:35:56 -0300

    Adding components to be used in creatures page

--- src/presentation/components/BottomBar/BottomBar.tsx
+++ src/presentation/components/BottomBar/BottomBar.tsx
@@ -1,4 +1,5 @@
 import { Grid } from '@material-ui/core';
+import { dolphinServiceLinks } from 'app/constants/contants';
 import useTranslation from 'app/hooks/useTranslation';
 import styles from './BottomBar.module.scss';
 
@@ -7,10 +8,14 @@ export const BottomBar = () => {
   return (
     <Grid container className={styles.bottomBar}>
       <Grid item xs={6} sm={4} md={2} lg={2}>
-        {t('bottomBar.terms')}
+        <a href={dolphinServiceLinks.termOfService} target="_blank">
+          {t('bottomBar.terms')}
+        </a>
       </Grid>
       <Grid item xs={6} sm={4} md={2} lg={2}>
-        {t('bottomBar.policy')}
+        <a href={dolphinServiceLinks.privacyPolicy} target="_blank">
+          {t('bottomBar.policy')}
+        </a>
       </Grid>
       <Grid item md={6} lg={6}></Grid>
       <Grid item xs={12} sm={4} md={2} lg={2} className={styles.bottomBar__brand}>

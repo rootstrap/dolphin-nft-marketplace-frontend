efaa39a8b1333e5e6b5b295328c5b86ea6a63023
Alejoboga20 <alejoboga20@gmail.com>
2021-11-19 18:57:13 -0300

    Adding little fixes

--- src/infrastructure/components/Layout/TopBarLayout.tsx
+++ src/infrastructure/components/Layout/TopBarLayout.tsx
@@ -7,15 +7,12 @@ import { TopBar } from 'presentation/components/TopBar/TopBar';
 import { BackgroundLayout } from './BackgroundLayout';
 import { CC } from 'presentation/components/CC/CC';
 import { KYC } from 'presentation/components/KYC/KYC';
-import useRedirection from 'app/hooks/useRedirection';
 
 interface TopBarLayoutProps {
   pageComponent: ReactElement;
 }
 
 export const TopBarLayout = ({ pageComponent }: TopBarLayoutProps) => {
-  const { isCreaturesUser } = useRedirection();
-
   return (
     <>
       <BackgroundLayout />
@@ -26,7 +23,7 @@ export const TopBarLayout = ({ pageComponent }: TopBarLayoutProps) => {
       <CreditCardModal />
       <CC />
       <KYC />
-      {!isCreaturesUser && <BottomBar />}
+      <BottomBar />
     </>
   );
 };

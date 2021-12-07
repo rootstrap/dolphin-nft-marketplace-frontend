0943edfdc11ef1d3259bb32ac5733907d7f253f4
Alejoboga20 <alejoboga20@gmail.com>
2021-12-07 16:11:41 -0300

    Keeping nft title

--- src/presentation/pages/Profile/MyCollection/Item.tsx
+++ src/presentation/pages/Profile/MyCollection/Item.tsx
@@ -1,31 +1,19 @@
-import { useState } from 'react';
 import { Typography } from '@material-ui/core';
 import useTranslation from 'app/hooks/useTranslation';
 import styles from './MyCollection.module.scss';
 
 export const Item = ({ id, name, totalQuantity, image, verticalId }: ItemProps) => {
   const t = useTranslation();
-  const [isVisible, setIsVisible] = useState<boolean>(false);
-  const handleMouseEnter = () => setIsVisible(true);
-  const handleMouseLeave = () => setIsVisible(false);
 
   return (
-    <div className={styles.list__itemContent} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+    <div className={styles.list__itemContent}>
       <img src={image} alt="Promotion" className={styles.list__itemContentImg} />
 
-      {isVisible && (
-        <div className={styles.list__itemContentHidden}>
-          <Typography className={styles.list__itemContentHiddenTitle} variant="h5" component="div">
-            {name}
-          </Typography>
-
-          <div className={styles.list__favContainer}>
-            <Typography>
-              {totalQuantity ? `${t('verticals.item.totalEdition')} ${totalQuantity}` : 'Single Item'}
-            </Typography>
-          </div>
-        </div>
-      )}
+      <div className={styles.list__itemContentHidden}>
+        <Typography className={styles.list__itemContentHiddenTitle} variant="h5" component="div">
+          {name}
+        </Typography>
+      </div>
     </div>
   );
 };

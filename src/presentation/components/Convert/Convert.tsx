73a960bebb66340854ab0ad9b5ad3be02d4981a3
Alejoboga20 <alejoboga20@gmail.com>
2021-12-10 14:30:16 -0300

    Adding convert component

--- src/presentation/components/Convert/Convert.tsx
+++ src/presentation/components/Convert/Convert.tsx
@@ -1,52 +1,115 @@
 import { Button, MenuItem, Typography } from '@material-ui/core';
-import { InputText } from 'infrastructure/components/InputText/InputText';
+import { useConvert } from './useConvert';
 import { InputSelect } from 'infrastructure/components/Select/InputSelect';
+import { InputText } from 'infrastructure/components/InputText/InputText';
 import styles from './Convert.module.scss';
-
-const options = [
-  {
-    name: 'name 1',
-  },
-  {
-    name: 'name 2',
-  },
-  {
-    name: 'name 3',
-  },
-];
+import { CustomLoader } from 'infrastructure/components/CustomLoader/CustomLoader';
+import useTranslation from 'app/hooks/useTranslation';
 
 export const Convert = () => {
+  const {
+    showResume,
+    isConvertExpired,
+    currencies,
+    handleSubmit,
+    onSubmit,
+    register,
+    errors,
+    getConvertData,
+    handleConfirm,
+    isLoading,
+    error,
+  } = useConvert();
+
+  const t = useTranslation();
+
   return (
-    <div className={styles.convert}>
-      <Typography gutterBottom variant="h6">
-        Convert your coins
-      </Typography>
-      <div className={styles.convert__fromCoin}>
-        <InputSelect>
-          {options.map(option => (
-            <MenuItem key={option.name} value={option.name}>
-              {option.name}
-            </MenuItem>
-          ))}
-        </InputSelect>
-      </div>
+    <form className={styles.convert} onSubmit={handleSubmit(onSubmit)}>
+      {isLoading ? (
+        <CustomLoader />
+      ) : (
+        <>
+          <div className={styles.convert__title}>
+            <Typography gutterBottom variant="h4">
+              {t('profile.convertCoins.title')}
+            </Typography>
+          </div>
 
-      <div className={styles.convert__toCoin}>
-        {options.map(option => (
-          <MenuItem key={option.name} value={option.name}>
-            {option.name}
-          </MenuItem>
-        ))}
-      </div>
+          {error && (
+            <div className={styles.convert__error}>
+              <Typography variant="h6">{error}</Typography>
+            </div>
+          )}
 
-      <div className={styles.convert__input}>
-        <InputText />
-      </div>
+          <div className={styles.convert__coinContainer}>
+            <div className={styles.convert__fromCoinContainer}>
+              <label htmlFor="fromCoin">{t('profile.convertCoins.from')}</label>
+              <InputSelect className="" register={register} name="fromCoin" error={errors.fromCoin}>
+                {currencies.map(currency => (
+                  <MenuItem value={currency} key={currency}>
+                    {currency}
+                  </MenuItem>
+                ))}
+              </InputSelect>
+            </div>
 
-      <div className={styles.convert__buttons}>
-        <Button>CANCEL</Button>
-        <Button>CONVERT</Button>
-      </div>
-    </div>
+            <div className={styles.convert__toCoinContainer}>
+              <label htmlFor="toCoin">{t('profile.convertCoins.to')}</label>
+
+              <InputSelect className="" register={register} name="toCoin" error={errors.toCoin}>
+                {currencies.map(currency => (
+                  <MenuItem value={currency} key={currency}>
+                    {currency}
+                  </MenuItem>
+                ))}
+              </InputSelect>
+            </div>
+          </div>
+
+          <div className={styles.convert__input}>
+            <div className={styles.convert__inputContainer}>
+              <label htmlFor="size">{t('profile.convertCoins.amount')}</label>
+              <InputText
+                inputProps={{ min: 0, style: { textAlign: 'center' } }}
+                className=""
+                register={register}
+                type="number"
+                name="size"
+                error={errors.size}
+              />
+            </div>
+          </div>
+          {showResume && (
+            <div>
+              <Typography>
+                {t('profile.convertCoins.cost')} {getConvertData?.cost}
+              </Typography>
+              <Typography>
+                {t('profile.convertCoins.price')} {getConvertData?.price}
+              </Typography>
+              <Typography>
+                {t('profile.convertCoins.proceeds')} {getConvertData?.proceeds}
+              </Typography>
+            </div>
+          )}
+
+          {!showResume && isConvertExpired && (
+            <div className={styles.convert__buttons}>
+              <Button type="submit">{t('profile.convertCoins.convertButton')}</Button>
+            </div>
+          )}
+
+          {!isConvertExpired && showResume && (
+            <>
+              <div className={styles.convert__buttons}>
+                <Button disabled={!showResume} onClick={handleConfirm}>
+                  {t('profile.convertCoins.confirmButton')}
+                </Button>
+              </div>
+            </>
+          )}
+        </>
+      )}
+    </form>
   );
 };
---
+++ src/presentation/components/Convert/useConvert.ts
@@ -0,0 +1,118 @@
+import { useEffect, useState } from 'react';
+import {
+  useConfirmConvertBalanceMutation,
+  useConvertBalanceMutation,
+  useGetBalanceMutation,
+  useGetCoinsMutation,
+  useGetConvertBalanceMutation,
+} from 'infrastructure/services/deposit/DepositService';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as z from 'zod';
+
+interface FormValues {
+  fromCoin: string;
+  toCoin: string;
+  size: string;
+}
+
+export const useConvert = () => {
+  const [showResume, setShowResume] = useState(false);
+  const [isConvertExpired, setIsConvertExpired] = useState(true);
+  const [currencies, setCurrencies] = useState<string[]>([]);
+  const [error, setError] = useState('');
+
+  const [getCoins, { data: getCoinsData, isSuccess: isGetCoinSuccess }] = useGetCoinsMutation();
+  const [getBalance] = useGetBalanceMutation();
+  const [
+    convertBalance,
+    { isSuccess: isConvertBalanceSuccess, isError: isConvertBalanceError, data, error: convertBalanceError },
+  ] = useConvertBalanceMutation();
+  const [confirmConvertBalance, { isSuccess: isConfirmConvertBalanceSuccess, isLoading }] =
+    useConfirmConvertBalanceMutation();
+  const [getConvertBalance, { isSuccess: isGetConvertBalanceSuccess, data: getConvertData }] =
+    useGetConvertBalanceMutation();
+
+  const schema = z
+    .object({
+      fromCoin: z.string().min(3, { message: 'Field Required' }),
+      toCoin: z.string().min(3, { message: 'Field Required' }),
+      size: z.string().min(1, { message: 'Field Required' }),
+    })
+    .refine(data => data.fromCoin !== data.toCoin, {
+      message: 'Coins must be different',
+      path: ['fromCoin', 'toCoin'],
+    });
+
+  const {
+    register,
+    handleSubmit,
+    clearErrors,
+    reset,
+    formState: { errors },
+  } = useForm({ resolver: zodResolver(schema) });
+
+  const onSubmit: SubmitHandler<FormValues> = async data => {
+    await convertBalance(data);
+  };
+
+  useEffect(() => {
+    getCoins();
+  }, [getCoins]);
+
+  useEffect(() => {
+    if (isConvertBalanceSuccess) {
+      setError('');
+      getConvertBalance(data.result.quoteId);
+    }
+  }, [isConvertBalanceSuccess]);
+
+  useEffect(() => {
+    if (isGetCoinSuccess) {
+      const currencies = getCoinsData.result.map(coin => coin.id);
+      setCurrencies(currencies);
+    }
+  }, [isGetCoinSuccess]);
+
+  useEffect(() => {
+    if (isGetConvertBalanceSuccess) {
+      setIsConvertExpired(false);
+      setShowResume(true);
+
+      setTimeout(() => {
+        setShowResume(false);
+
+        setIsConvertExpired(true);
+      }, 4000);
+    }
+  }, [isGetConvertBalanceSuccess]);
+
+  useEffect(() => {
+    if (isConfirmConvertBalanceSuccess) {
+      getBalance();
+    }
+  }, [isConfirmConvertBalanceSuccess, getBalance]);
+
+  useEffect(() => {
+    if (isConvertBalanceError) {
+      const error: any = Object(convertBalanceError);
+      setError(error.data.error);
+    }
+  }, [isConvertBalanceError]);
+
+  const handleConfirm = () => confirmConvertBalance(data.result.quoteId);
+
+  return {
+    showResume,
+    currencies,
+    handleSubmit,
+    onSubmit,
+    register,
+    errors,
+    getConvertData,
+    isConvertExpired,
+    handleConfirm,
+    isLoading,
+    error,
+  };
+};

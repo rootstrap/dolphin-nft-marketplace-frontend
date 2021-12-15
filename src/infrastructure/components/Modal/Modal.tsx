24a1a59569a0852e9dc7cf70de7929f9b3c04ee2
Gaston Rivata <gastonrivata@gmail.com>
2021-12-15 10:41:44 -0300

    Changing close svg to mui icon

--- src/infrastructure/components/Modal/Modal.tsx
+++ src/infrastructure/components/Modal/Modal.tsx
@@ -1,12 +1,12 @@
 import { Modal } from '@material-ui/core';
-import closeBtn from 'app/assets/close.svg';
+import CloseIcon from '@material-ui/icons/Close';
 import styles from './Modal.module.scss';
 
 export const BaseModal = ({ open, handleClose, children, bigModal = false }: ModalProps) => (
-  <Modal open={open} onClose={handleClose} style={{}} disableScrollLock>
+  <Modal open={open} onClose={handleClose} disableScrollLock>
     <div className={styles.modal} style={bigModal ? { width: '70%' } : {}}>
-      <button className={styles.modal__close} onClick={handleClose} type="button">
-        <img src={closeBtn} alt="close" />
+      <button className={styles.modal__close} onClick={handleClose}>
+        <CloseIcon color="primary" fontSize="large" />
       </button>
       {children}
     </div>

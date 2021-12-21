566369642ea859bd226d8a3ca8147a6619d69c0a
Alejoboga20 <alejoboga20@gmail.com>
2021-12-21 16:51:36 -0300

    Updating carousel info

--- src/app/constants/creatures/carouselItems.ts
+++ src/app/constants/creatures/carouselItems.ts
@@ -3,13 +3,13 @@ import Civilians from 'app/assets/Civilians.png';
 import Engineers from 'app/assets/Engineers.png';
 import Royals from 'app/assets/Royals.png';
 import Elders from 'app/assets/Elders.png';
-import Assassin from 'app/assets/Assassin.png';
+import SpecialForces from 'app/assets/SpecialForces.png';
 
 export const carouselItems = [
   {
     id: 0,
     src: Monk,
-    name: 'Monk',
+    name: 'Techno Monks',
     banner: '????',
     description: '????',
     characteristics: '????',
@@ -18,48 +18,51 @@ export const carouselItems = [
   {
     id: 1,
     src: Civilians,
-    name: 'Civilians',
+    name: 'The VOX',
     banner: 'Strong-Willed Survivors',
     description:
-      'The heart and soul of their endangered species, they  are far more than mere faces in the crowd. Although their education is limited to the specific trade that has distinguished each of their respective families for generations, those particular skills and talents are vital to their species’ continued existence. They are the ideators and imagineers whose craft fill the ⏃⏁⍀⟟⎐⟟⏃⋏⌇daily lives with substance',
-    characteristics: '????',
+      'The heart and soul of their endangered species, they are far more than mere faces in the crowd. Although their education is limited to the specific trade that has distinguished each of their respective families for generations, those particular skills and talents are vital to their species’ continued existence. They are the ideators and imagineers whose craft fill the aliens’ daily lives with substance.',
+    characteristics: 'Hands in front of body',
     rarity: '????',
   },
   {
     id: 2,
     src: Engineers,
-    name: 'Engineers',
+    name: 'The VERSAI',
     banner: 'The Methodical Makers',
-    description: '????',
-    characteristics: 'Technical Engineering Suit',
+    description:
+      'Schooled in the sacred sciences, the Elders tasked them with advancing technology. They are the architects and inventors. The technicians and the programmers. They accept the power of the Glyph but honor the wisdom of the Dhantari',
+    characteristics: 'Technical Engineering Suit with Collar',
     rarity: '????',
   },
   {
     id: 3,
     src: Royals,
-    name: 'Royals',
+    name: 'The GLYPH',
     banner: 'The Power Elite',
     description:
-      'Descendants of the first ⏃⏁⍀⟟⎐⟟⏃⋏  Kings and Queens, they are the light to which all others look to for guidance and hope. They are fashionable and polished, often clad in jewels and fineries. Some are determined to restore their species to its former greatness. Others are more intent on restoring the privileges of their former lifestyle no matter the cost',
-    characteristics: 'Horns/Crowns',
+      'Descendants of the first Kings and Queens, they are the light to which all others look to for guidance and hope. They are fashionable and polished, often clad in jewels and fineries. Some are determined to restore their species to its former greatness. Others are more intent on restoring the privileges of their former lifestyle no matter the cost.',
+    characteristics: 'Technical skin patterns on body',
     rarity: '????',
   },
   {
     id: 4,
     src: Elders,
-    name: 'Elders',
-    banner: 'Strong-Willed Survivors',
-    description: '????',
-    characteristics: '????',
+    name: 'The DHANTARI',
+    banner: 'The Wisest',
+    description:
+      'Guardians of their species’ extraordinary knowledge and innovations, the Elders are the architects of their species’ survival strategy. Their brilliance and cunning is why they are revered. Their valuation of rationality over morality is why they are feared.',
+    characteristics: 'Dragon Heads + Tentacle Face Heads',
     rarity: '????',
   },
   {
     id: 5,
-    src: Assassin,
-    name: 'Assassin',
-    banner: '????',
-    description: '????',
-    characteristics: 'Body Tattoo/Assassin Crests/Weapons',
+    src: SpecialForces,
+    name: 'The ABAKADA TIMOR',
+    banner: 'The Skilled Specialists',
+    description:
+      'As driven by their expertise as they are defined by it, the Special Forces serve their species with equal parts pride and prowess. They are the aliens’ first line of defense and willing to sacrifice so that others might survive. They are the warriors and the soldiers, the pilots and the police force.',
+    characteristics: 'Uniforms',
     rarity: '????',
   },
 ];

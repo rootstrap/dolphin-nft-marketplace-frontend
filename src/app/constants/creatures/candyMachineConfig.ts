b9db0fdd6c7773d37a9c52509bb6713bcd19e25b
Alejo Boga <60139228+Alejoboga20@users.noreply.github.com>
2021-12-23 16:51:11 -0300

    Updating candy machine config

--- src/app/constants/creatures/candyMachineConfig.ts
+++ src/app/constants/creatures/candyMachineConfig.ts
@@ -1,7 +1,7 @@
 import * as anchor from '@project-serum/anchor';
 
 export const candyMachineConfig = {
-  connection: new anchor.web3.Connection('https://api.devnet.solana.com'),
+  connection: new anchor.web3.Connection('https://api.mainnet-beta.solana.com'),
   treasury: new anchor.web3.PublicKey('AfabEY9MxXDWJQM4vAn8CLdCLEMzVK8o89CiMq5gGDkU'),
   config: new anchor.web3.PublicKey('9gKptEZr2uMnMnfPxFUb4VNFLQX81jsfBmipoTrTChHY'),
   candyMachineId: new anchor.web3.PublicKey('bxMV8Stg9qgqaHzPxcLR6BRb2PpKvoPwQXU2ueHMqmE'),

8b38cf4dd1399df6677a15dccfa28d1214de57fc
Alejoboga20 <alejoboga20@gmail.com>
2021-12-29 10:19:25 -0300

    Removing useless component and reorganizing folder

--- src/presentation/pages/Creatures/CreaturesPage.tsx
+++ src/presentation/pages/Creatures/CreaturesPage.tsx
@@ -1,12 +1,25 @@
 import { ThemeProvider } from '@material-ui/core';
 import { creaturesTheme } from 'app/themes/CreaturesTheme';
 import { TopBarLayout } from 'infrastructure/components/Layout/TopBarLayout';
-import { Main } from './Main/Main';
+import { FaqAccordion } from 'presentation/components/FAQAccordion/FaqAccordion';
+import { CreaturesCarousel, KnowUs, Main, Perks, Social, StartMap } from '../Creatures';
 
 const CreaturesPage = () => {
   return (
     <ThemeProvider theme={creaturesTheme}>
-      <TopBarLayout pageComponent={<Main />} />
+      <TopBarLayout
+        pageComponent={
+          <>
+            <Main />
+            <CreaturesCarousel />
+            <KnowUs />
+            <Perks />
+            <StartMap />
+            <FaqAccordion />
+            <Social />
+          </>
+        }
+      />
     </ThemeProvider>
   );
 };
---
+++ src/presentation/pages/Creatures/index.ts
@@ -0,0 +1,6 @@
+export { CreaturesCarousel } from './Carousel/CreaturesCarousel';
+export { KnowUs } from './KnowUs/KnowUs';
+export { Main } from './Main/Main';
+export { Perks } from './Perks/Perks';
+export { Social } from './Social/Social';
+export { StartMap } from './StartMap/StartMap';

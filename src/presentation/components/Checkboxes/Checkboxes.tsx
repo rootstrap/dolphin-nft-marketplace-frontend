829cb263a2d49a7ddae3cb985c9ffa26187146ef
Gaston Rivata <gastonrivata@gmail.com>
2022-01-10 15:50:29 -0300

    Adding new modal for checkboxes component

--- src/presentation/components/Checkboxes/Checkboxes.tsx
+++ src/presentation/components/Checkboxes/Checkboxes.tsx
@@ -1,77 +1,73 @@
-import { useEffect } from 'react';
 import { Button, Checkbox, Typography } from '@material-ui/core';
 import { useCheckboxes } from './useCheckboxes';
 import { CustomLoader } from 'infrastructure/components/CustomLoader/CustomLoader';
 import useTranslation from 'app/hooks/useTranslation';
+import { BaseModal } from 'infrastructure/components/Modal/Modal';
 import styles from './Checkboxes.module.scss';
 
-export const Checkboxes = ({ handleClose, error }: CheckboxesProps) => {
+export const Checkboxes = ({ error }: CheckboxesProps) => {
   const t = useTranslation();
-  const { kyc1ed, isWalletReady, handleOnClick, isLoading } = useCheckboxes();
-
-  useEffect(() => {
-    if (isWalletReady) {
-      handleClose();
-    }
-  }, [isWalletReady]);
+  const { kyc1ed, isWalletReady, handleOnClick, isLoading, checkboxesModalIsOpen, handleClose } =
+    useCheckboxes();
 
   return (
-    <div className={styles.checkboxes}>
-      {isLoading ? (
-        <div className={styles.checkboxes__spinner}>
-          <CustomLoader />
-        </div>
-      ) : (
-        <>
-          <div className={styles.checkboxes__title}>
-            <Typography gutterBottom variant="h4">
-              {t('walletSetup.title')}
-            </Typography>
-            <Typography gutterBottom variant="h5">
-              {t('walletSetup.subtitle')}
-            </Typography>
-            <Typography gutterBottom variant="h6">
-              {t('walletSetup.description')}
-            </Typography>
+    <BaseModal open={checkboxesModalIsOpen} handleClose={handleClose}>
+      <div className={styles.checkboxes}>
+        {isLoading ? (
+          <div className={styles.checkboxes__spinner}>
+            <CustomLoader />
           </div>
+        ) : (
+          <>
+            <div className={styles.checkboxes__title}>
+              <Typography gutterBottom variant="h4">
+                {t('walletSetup.title')}
+              </Typography>
+              <Typography gutterBottom variant="h5">
+                {t('walletSetup.subtitle')}
+              </Typography>
+              <Typography gutterBottom variant="h6">
+                {t('walletSetup.description')}
+              </Typography>
+            </div>
 
-          {error && <div>{error}</div>}
+            {error && <div>{error}</div>}
 
-          <div className={styles.checkboxes__container}>
-            <Checkbox checked />
-            <Typography variant="h6" className={styles.checkboxes__containerText}>
-              {t('walletSetup.firstCheck')}
+            <div className={styles.checkboxes__container}>
+              <Checkbox checked />
+              <Typography variant="h6" className={styles.checkboxes__containerText}>
+                {t('walletSetup.firstCheck')}
+              </Typography>
+            </div>
+
+            <div className={styles.checkboxes__container}>
+              <Checkbox checked={Boolean(kyc1ed)} />
+              <Typography variant="h6" className={styles.checkboxes__containerText}>
+                {t('walletSetup.secondCheck')}
+              </Typography>
+            </div>
+
+            <div className={styles.checkboxes__container}>
+              <Checkbox checked={isWalletReady} />
+              <Typography variant="h6" className={styles.checkboxes__containerText}>
+                {t('walletSetup.thirdCheck')}
+              </Typography>
+            </div>
+
+            <div className={styles.checkboxes__button}>
+              <Button onClick={handleOnClick}>{t('walletSetup.activate')}</Button>
+            </div>
+
+            <Typography variant="body1" onClick={handleClose} className={styles.checkboxes__remind}>
+              {t('walletSetup.remindMeLater')}
             </Typography>
-          </div>
-
-          <div className={styles.checkboxes__container}>
-            <Checkbox checked={Boolean(kyc1ed)} />
-            <Typography variant="h6" className={styles.checkboxes__containerText}>
-              {t('walletSetup.secondCheck')}
-            </Typography>
-          </div>
-
-          <div className={styles.checkboxes__container}>
-            <Checkbox checked={isWalletReady} />
-            <Typography variant="h6" className={styles.checkboxes__containerText}>
-              {t('walletSetup.thirdCheck')}
-            </Typography>
-          </div>
-
-          <div className={styles.checkboxes__button}>
-            <Button onClick={handleOnClick}>{t('walletSetup.activate')}</Button>
-          </div>
-
-          <Typography variant="body1" onClick={handleClose} className={styles.checkboxes__remind}>
-            {t('walletSetup.remindMeLater')}
-          </Typography>
-        </>
-      )}
-    </div>
+          </>
+        )}
+      </div>
+    </BaseModal>
   );
 };
 
 interface CheckboxesProps {
-  handleClose: () => void;
   error?: string;
 }
--- src/presentation/components/Checkboxes/useCheckboxes.ts
+++ src/presentation/components/Checkboxes/useCheckboxes.ts
@@ -1,27 +1,33 @@
-import { useContext, useEffect, useState } from 'react';
+import { useCallback, useContext, useEffect, useState } from 'react';
 import { useAppSelector } from 'app/hooks/reduxHooks';
-import { useLoginStatusMutation } from 'infrastructure/services/user/UserService';
 import { ModalContext } from 'app/context/ModalContext';
 import { useGetBalanceMutation } from 'infrastructure/services/deposit/DepositService';
 import { useGetCreditCardsMutation } from 'infrastructure/services/creditCard/CreditCardService';
 
 export const useCheckboxes = () => {
   const [isLoading, setIsLoading] = useState(false);
+  const [isWalletReady, setIsWalletReady] = useState(false);
+
   const { kyc1ed } = useAppSelector(state => state.user.user);
   const { defaultCreditCard } = useAppSelector(state => state.creditCard);
+
+  const { checkboxesModalIsOpen, setCheckboxesModalIsOpen } = useContext(ModalContext);
   const { setKycModalIsOpen, setCcModalIsOpen } = useContext(ModalContext);
+
   const [getCreditCards] = useGetCreditCardsMutation();
   const [getBalance] = useGetBalanceMutation();
-  const [loginStatus] = useLoginStatusMutation();
-
-  const isWalletReady = defaultCreditCard.status === 'approved';
 
   const handleOnClick = () => {
     kyc1ed ? setCcModalIsOpen(true) : setKycModalIsOpen(true);
   };
 
-  const loadUserData = async () => {
+  const handleClose = () => {
+    setCheckboxesModalIsOpen(false);
+  };
+
+  const loadUserData = useCallback(async () => {
     setIsLoading(true);
+    console.log('sarasa');
     try {
       await getCreditCards();
       await getBalance();
@@ -30,20 +36,22 @@ export const useCheckboxes = () => {
     } finally {
       setIsLoading(false);
     }
-  };
-
-  useEffect(() => {
-    loginStatus();
-  }, [kyc1ed]);
+  }, [getCreditCards, getBalance]);
 
   useEffect(() => {
     loadUserData();
-  }, []);
+  }, [loadUserData]);
+
+  useEffect(() => {
+    setIsWalletReady(defaultCreditCard.status === 'approved');
+  }, [defaultCreditCard]);
 
   return {
     kyc1ed,
     isWalletReady,
     handleOnClick,
     isLoading,
+    handleClose,
+    checkboxesModalIsOpen,
   };
 };

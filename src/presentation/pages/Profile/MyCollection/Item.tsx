09dd0e22d3cb07eb3ccd6faab5646438bae2257f
Alejo Boga <alejoboga20@gmail.com>
2022-01-12 16:20:33 -0300

    Adding style to item and linking to the new page

--- src/presentation/pages/Profile/MyCollection/Item.tsx
+++ src/presentation/pages/Profile/MyCollection/Item.tsx
@@ -1,20 +1,23 @@
 import { Typography } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 import useTranslation from 'app/hooks/useTranslation';
 import styles from './MyCollection.module.scss';
 
-export const Item = ({ id, name, totalQuantity, image, verticalId }: ItemProps) => {
+export const Item = ({ id, name, image }: ItemProps) => {
   const t = useTranslation();
 
   return (
-    <div className={styles.list__itemContent}>
-      <img src={image} alt="Promotion" className={styles.list__itemContentImg} />
+    <Link to={`secondary/${id}`}>
+      <div className={styles.list__itemContent}>
+        <img src={image} alt="Promotion" className={styles.list__itemContentImg} />
 
-      <div className={styles.list__itemContentHidden}>
-        <Typography className={styles.list__itemContentHiddenTitle} variant="h5" component="div">
-          {name}
-        </Typography>
+        <div className={styles.list__itemContentHidden}>
+          <Typography className={styles.list__itemContentHiddenTitle} variant="h5" component="div">
+            {name}
+          </Typography>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 

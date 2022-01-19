ba2c65bbafa08535d76303899c29b6473829342d
Alejo Boga <alejoboga20@gmail.com>
2022-01-19 10:31:01 -0300

    Changing item description

--- src/app/constants/athletes/remarkablesCarousel.ts
+++ src/app/constants/athletes/remarkablesCarousel.ts
@@ -4,7 +4,7 @@ export const carouselItems: CarouselItem[] = [
   {
     title: 'Winter Sports Champions',
     date: 'Coming February 2022',
-    text: 'This action packed collection features a combination of decorated legends and current winter athlete stars, including many current competitors in the February games like gold medalists Red Gerard, Hilary Knight and Kendall Coyne, along with skating legend Nancy Kerrigan. IRLs for this collection include experiences that you can’t get anywhere else! IRLs may include a luge ride with silver medalist Chris Mazdzer, and many more',
+    text: 'This action packed collection features a combination of decorated legends and current winter athlete stars, including many current competitors in the February games like gold medalists Red Gerard, Hilary Knight and Kendall Coyne, along with skating legend Nancy Kerrigan. IRLs for this collection include experiences that you can’t get anywhere else! IRLs may include a luge ride with Chris Mazdzer, hand-picked sports gear signed by the pros, and much more!',
     collectiblesPerPack: 3,
     costPerPack: 3,
     tier: 'Gold',

87b9f360cff97df03f99cf45db92e7af95e9057f
Gaston Rivata <gastonrivata@gmail.com>
2022-01-19 17:23:52 -0300

    Changing remarkables background images

--- src/presentation/pages/Remarkables/Main/Main.tsx
+++ src/presentation/pages/Remarkables/Main/Main.tsx
@@ -1,31 +1,43 @@
-import { Button, Link, Typography } from '@material-ui/core';
+import { Button, Grid, Link, Typography } from '@material-ui/core';
 import useTranslation from 'app/hooks/useTranslation';
 import styles from './Main.module.scss';
 import { HowItWorks } from '../HowItWorks/HowItWorks';
-import { BackgroundImageLayout } from 'infrastructure/components/BackgroundImageLayout/BackgroundImageLayout';
 import { AthElitesCarousel } from 'presentation/components/AthElitesCarousel/AthElitesCarousel';
 import { carouselItems } from 'app/constants/athletes/remarkablesCarousel';
+import Remarkable1 from 'app/assets/remarkable1.png';
+import Remarkable2 from 'app/assets/remarkable2.png';
+import Remarkable3 from 'app/assets/remarkable3.png';
 
 export const Main = () => {
   const t = useTranslation();
 
   return (
     <>
-      <BackgroundImageLayout>
-        <div className={styles.mainContent__header}>
-          <Typography component="div" variant="h2" className={styles.mainContent__headerTitle}>
-            {t('remarkables.title')}
-          </Typography>
-          <Typography component="div" variant="subtitle1" className={styles.mainContent__headerTeaser}>
-            {t('remarkables.teaser')}
-          </Typography>
-          <div>
-            <Link href="#athelites" underline="none">
-              <Button className={styles.mainContent__headerButton}> {t('verticals.actionButton')}</Button>
-            </Link>
-          </div>
+      <div className={styles.mainContent__header}>
+        <Typography component="div" variant="h2" className={styles.mainContent__headerTitle}>
+          {t('remarkables.title')}
+        </Typography>
+        <Typography component="div" variant="subtitle1" className={styles.mainContent__headerTeaser}>
+          {t('remarkables.teaser')}
+        </Typography>
+        <div>
+          <Link href="#athelites" underline="none">
+            <Button className={styles.mainContent__headerButton}> {t('verticals.actionButton')}</Button>
+          </Link>
         </div>
-      </BackgroundImageLayout>
+      </div>
+      <Grid container className={styles.mainContent__background}>
+        <Grid item md={4}>
+          <img alt="" src={Remarkable1} className={styles.mainContent__backgroundImg} />
+        </Grid>
+        <Grid item md={4}>
+          <img alt="" src={Remarkable2} className={styles.mainContent__backgroundImg} />
+        </Grid>
+        <Grid item md={4}>
+          <img alt="" src={Remarkable3} className={styles.mainContent__backgroundImg} />
+        </Grid>
+      </Grid>
+
       <HowItWorks />
       <AthElitesCarousel carouselItems={carouselItems} />
     </>

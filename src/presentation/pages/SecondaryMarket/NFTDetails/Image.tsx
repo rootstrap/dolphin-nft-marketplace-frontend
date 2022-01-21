56d77d82fad6ea1f4d3e00383296d9d19384a3a6
Alejo Boga <alejoboga20@gmail.com>
2022-01-21 12:40:29 -0300

    Adding fixes

--- src/presentation/pages/SecondaryMarket/NFTDetails/Image.tsx
+++ src/presentation/pages/SecondaryMarket/NFTDetails/Image.tsx
@@ -8,20 +8,29 @@ export const Image = () => {
     <div className={styles.secondaryMarket__nft}>
       <span className={styles.secondaryMarket__nftEditionContainer}>
         <span className={styles.secondaryMarket__nftEdition}>#{nft?.number}</span>
-        <span className={`${styles.secondaryMarket__nftEdition} ${styles.secondaryMarket__nftTotalEditions}`}>
-          {' '}
-          / {nft?.totalQuantity}
-        </span>
+        {nft?.totalQuantity && (
+          <span
+            className={`${styles.secondaryMarket__nftEdition} ${styles.secondaryMarket__nftTotalEditions}`}
+          >
+            {' '}
+            / {nft?.totalQuantity}
+          </span>
+        )}
       </span>
-      <iframe
-        style={{ border: 'none' }}
-        height="544"
-        width="544"
-        allow="autoplay; encrypted-media;"
-        allowFullScreen={true}
-        src={`${nft?.videoUrl}?autoplay=true&muted=true`}
-        title={nft?.name}
-      />
+
+      {nft?.videoUrl ? (
+        <iframe
+          style={{ border: 'none' }}
+          height="544"
+          width="544"
+          allow="autoplay; encrypted-media;"
+          allowFullScreen={true}
+          src={`${nft?.videoUrl}?autoplay=true&muted=true`}
+          title={nft?.name}
+        />
+      ) : (
+        <img className={styles.secondaryMarket__nftImg} src={nft?.imageUrl} alt="" />
+      )}
     </div>
   );
 };

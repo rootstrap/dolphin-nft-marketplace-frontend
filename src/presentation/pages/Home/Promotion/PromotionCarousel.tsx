9e3e22de97180b19db8c523d0e8736a9df0db6f5
Gaston Rivata <gastonrivata@gmail.com>
2022-02-02 11:04:51 -0300

    Adding carousel component to home page

--- src/presentation/pages/Home/Promotion/PromotionCarousel.tsx
+++ src/presentation/pages/Home/Promotion/PromotionCarousel.tsx
@@ -1,33 +1,28 @@
 import { Grid } from '@material-ui/core';
 import { Item } from './Item';
 import { usePromotion } from './usePromotion';
-import { NFT } from 'app/interfaces/NFT/NFT';
-import { Arrow } from 'infrastructure/components/Arrow/Arrow';
+import { Carousel } from 'infrastructure/components/Carousel/Carousel';
 import styles from './Promotion.module.scss';
 
 export const PromotionCarousel = () => {
-  const { carouselContent, handleOnClick } = usePromotion();
+  const { nfts } = usePromotion();
 
   return (
     <Grid container justifyContent="center" className={styles.promotionContent__carouselItems}>
-      {carouselContent && (
-        <>
-          <Arrow handleOnClick={() => handleOnClick('left')} direction="left" />
-          {carouselContent.map((item: NFT) => (
-            <Grid item xs={12} md={5} lg={5} key={item.id}>
-              <Item
-                styles={styles}
-                id={item.id}
-                name={item.name}
-                totalQuantity={item.totalQuantity}
-                image={item.imageUrl}
-                price={item.offerPrice}
-                ftxId={item.ftx_id}
-              />
-            </Grid>
+      {nfts && (
+        <Carousel show={2} infiniteLoop>
+          {nfts.map(nft => (
+            <Item
+              id={nft.id}
+              name={nft.name}
+              totalQuantity={nft.totalQuantity}
+              image={nft.imageUrl}
+              price={nft.offerPrice}
+              ftxId={nft.ftx_id}
+              styles={styles}
+            />
           ))}
-          <Arrow handleOnClick={() => handleOnClick('right')} direction="right" />
-        </>
+        </Carousel>
       )}
     </Grid>
   );
--- src/presentation/pages/Home/Promotion/usePromotion.ts
+++ src/presentation/pages/Home/Promotion/usePromotion.ts
@@ -1,31 +1,15 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useGetNftsFeaturedMutation } from 'infrastructure/services/nft/NftService';
 import { NFT } from 'app/interfaces/NFT/NFT';
 
 export const usePromotion = () => {
   const [getNftsFeatured, { isError, isLoading, isSuccess }] = useGetNftsFeaturedMutation();
-  const [primaryIndex, setPrimaryIndex] = useState(0);
-  const [secondaryIndex, setSecondaryIndex] = useState(1);
   const [nfts, setNfts] = useState<NFT[]>([]);
 
-  const carouselContent = nfts.filter((nft, index) => index === primaryIndex || index === secondaryIndex);
-
-  const handleOnClick = (direction: 'right' | 'left') => {
-    const increment = direction === 'right' ? 1 : -1;
-
-    const numOfItems = nfts.length;
-
-    const newPrimaryIndex = (primaryIndex + increment + numOfItems) % numOfItems;
-    const newSecondaryIndex = newPrimaryIndex === numOfItems - 1 ? 0 : newPrimaryIndex + 1;
-
-    setPrimaryIndex(newPrimaryIndex);
-    setSecondaryIndex(newSecondaryIndex);
-  };
-
-  const loadData = async () => {
+  const loadData = useCallback(async () => {
     const data: any = await getNftsFeatured();
     setNfts(data.data);
-  };
+  }, [getNftsFeatured, setNfts]);
 
   useEffect(() => {
     loadData();
@@ -33,14 +17,12 @@ export const usePromotion = () => {
     return () => {
       window.scrollTo(0, 0);
     };
-  }, []);
+  }, [loadData]);
 
   return {
     nfts,
     isLoading,
     isError,
     isSuccess,
-    carouselContent,
-    handleOnClick,
   };
 };

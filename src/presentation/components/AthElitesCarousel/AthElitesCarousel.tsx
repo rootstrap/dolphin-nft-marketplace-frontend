c9cb74df55d704b184de2b0adf25ef38cbb4b263
Alejo Boga <alejoboga20@gmail.com>
2022-02-03 10:00:11 -0300

    Implementing buy now button in heroletes carousel

--- src/presentation/components/AthElitesCarousel/AthElitesCarousel.tsx
+++ src/presentation/components/AthElitesCarousel/AthElitesCarousel.tsx
@@ -1,13 +1,15 @@
-import { Button } from '@material-ui/core';
-import { CarouselItem } from 'app/constants/heroletes/remarkablesCarousel';
+import { CarouselItem, packIds } from 'app/constants/heroletes/remarkablesCarousel';
 import { CarouselItemDescription } from './CarouselItemDescription';
 import { CarouselItemImage } from './CarouselItemImage';
 import { CarouselButtons } from './CarouselButtons';
 import { useCarousel } from './useCarousel';
+import { BuyNowButton } from '../BuyNowButton/BuyNowButton';
+import { useAppSelector } from 'app/hooks/reduxHooks';
 import styles from './AthElitesCarousel.module.scss';
 
 export const AthElitesCarousel = ({ carouselItems }: CarouselProps) => {
   const { index, handleOnClick, areButtonsVisible } = useCarousel(carouselItems);
+  const { isAuthenticated } = useAppSelector(state => state.user);
 
   return (
     <>
@@ -24,13 +26,16 @@ export const AthElitesCarousel = ({ carouselItems }: CarouselProps) => {
         <CarouselItemImage tier={carouselItems[index].tier} image={carouselItems[index].image} />
       </div>
 
-      {areButtonsVisible ? (
-        <CarouselButtons handleOnClick={handleOnClick} />
-      ) : (
-        <div className={styles.carousel__buttons}>
-          <Button variant="contained" size="large" fullWidth>
-            Buy Now
-          </Button>
+      {areButtonsVisible && <CarouselButtons handleOnClick={handleOnClick} />}
+
+      {isAuthenticated && (
+        <div className={styles.carousel__buy}>
+          <BuyNowButton
+            buttonText="Buy Now"
+            className={styles.carousel__buyButton}
+            nftsToBuy="common"
+            packId={packIds.common}
+          />
         </div>
       )}
     </>

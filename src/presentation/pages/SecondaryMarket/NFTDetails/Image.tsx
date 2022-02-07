0e6a9e3f80b44ee6128820d3731873dfdf424e35
Alejo Boga <alejoboga20@gmail.com>
2022-02-07 09:38:14 -0300

    Refactoring component to be used by heroletes nfts

--- src/presentation/pages/SecondaryMarket/NFTDetails/Image.tsx
+++ src/presentation/pages/SecondaryMarket/NFTDetails/Image.tsx
@@ -1,9 +1,11 @@
+/* eslint-disable jsx-a11y/media-has-caption */
 import { useContext } from 'react';
 import { NFTDetailsContext } from './NFTDetails';
 import styles from './NftDetails.module.scss';
 
 export const Image = () => {
   const { nft } = useContext(NFTDetailsContext);
+
   return (
     <div className={styles.secondaryMarket__nft}>
       <span className={styles.secondaryMarket__nftEditionContainer}>
@@ -28,6 +30,10 @@ export const Image = () => {
           src={`${nft?.videoUrl}?autoplay=true&muted=true&loop=true`}
           title={nft?.name}
         />
+      ) : nft?.animationUrl ? (
+        <video className={styles.list__itemContentImg} height="544" width="544" muted autoPlay>
+          <source src={nft?.animationUrl} type="video/mp4" />
+        </video>
       ) : (
         <img className={styles.secondaryMarket__nftImg} src={nft?.imageUrl} alt="" />
       )}

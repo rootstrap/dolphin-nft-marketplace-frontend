ad0a07b013c9be284cefc719252dde31f0d8e7eb
Alejo Boga <alejoboga20@gmail.com>
2022-02-18 10:41:00 -0300

    Adding service to het NFTs with pagination and to get heroletes attributes from backend

--- src/app/interfaces/NFT/NFT.ts
+++ src/app/interfaces/NFT/NFT.ts
@@ -1,6 +1,7 @@
 export interface NFT {
   id: string;
   name: string;
+  attributes: Attributes;
   ftx_id: string;
   description: string;
   tier: string;
@@ -33,6 +34,16 @@ export interface FillsResult {
 export interface IFillsResponse {
   success: boolean;
   result: FillsResult[];
+}
+
+export interface Attributes {
+  Athlete: string;
+  Background: string;
+  Collection: string;
+  Signed: string;
+  Sport: string;
+  Tier: string;
+  imageUrl: string;
 }
 
 export type nftPack = 'common' | 'bronze' | 'silver' | 'gold' | 'creatures';

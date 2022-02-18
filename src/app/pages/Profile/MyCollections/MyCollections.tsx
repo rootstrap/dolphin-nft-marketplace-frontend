05b28afd3c9734441d79121a0462e594399c6c42
Alejo Boga <alejoboga20@gmail.com>
2022-02-18 10:50:01 -0300

    Adding new component

---
+++ src/app/pages/Profile/MyCollections/MyCollections.tsx
@@ -0,0 +1,39 @@
+import { Button, Typography } from '@material-ui/core';
+import { LockOutlined } from '@material-ui/icons';
+import { winterGridItems } from 'app/constants/heroletes/winterGridItems';
+import { MyCollectionItem } from './MyCollectionItem';
+import useTranslation from 'app/hooks/useTranslation';
+import styles from './MyCollection.module.scss';
+
+export const MyCollections = () => {
+  const t = useTranslation();
+
+  return (
+    <div className={styles.myCollection}>
+      <div className={styles.myCollection__title}>
+        <Typography variant="h4">{t('profile.myCollections.title')}</Typography>
+      </div>
+
+      <div className={styles.myCollection__subtitle}>
+        <Typography variant="h6">{t('profile.myCollections.subtitle')}</Typography>{' '}
+        <Typography variant="subtitle1" className={styles.myCollection__subtitleIRL}>
+          IRL
+        </Typography>
+      </div>
+
+      <div className={styles.myCollection__info}>
+        <div className={styles.myCollection__infoText}>
+          <Typography variant="body1">{t('profile.myCollections.infoText')}</Typography>
+        </div>
+
+        <div className={styles.myCollection__infoButton}>
+          <Button endIcon={<LockOutlined />}>{t('profile.myCollections.redeemButton')}</Button>
+        </div>
+      </div>
+
+      {winterGridItems.map((item, index) => (
+        <MyCollectionItem {...item} index={index} />
+      ))}
+    </div>
+  );
+};
--- src/app/pages/Profile/MyGallery/MyGallery.tsx
+++ src/app/pages/Profile/MyGallery/MyGallery.tsx
@@ -1,20 +1,22 @@
-import { useMyGallery } from './useMyGallery';
+import { useContext } from 'react';
+import { ProfileContext } from '../ProfilePage';
 import { CustomLoader } from 'app/components/CustomLoader/CustomLoader';
 import { GalleryList } from './GalleryList';
 import styles from './MyGallery.module.scss';
 
 export const MyGallery = () => {
-  const { nfts, isLoading } = useMyGallery();
+  const { nfts, isLoading } = useContext(ProfileContext);
+
+  if (isLoading)
+    return (
+      <div className={styles.myCollection__spinner}>
+        <CustomLoader />
+      </div>
+    );
 
   return (
     <div className={styles.myCollection}>
-      {isLoading ? (
-        <div className={styles.myCollection__spinner}>
-          <CustomLoader />
-        </div>
-      ) : (
-        <GalleryList nfts={nfts} />
-      )}
+      <GalleryList nfts={nfts} />
     </div>
   );
 };

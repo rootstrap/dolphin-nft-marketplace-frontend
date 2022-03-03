1b8341a863bfbe0fa26483f7cfbb79d3872d0879
Gaston Rivata <gastonrivata@gmail.com>
2022-03-03 09:44:50 -0300

    Adding athlete filter on herolete through clicking on winter grid

--- src/app/pages/Heroletes/WinterGrid/WinterGrid.tsx
+++ src/app/pages/Heroletes/WinterGrid/WinterGrid.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Grid, Typography } from '@material-ui/core';
 import { winterGridItems } from 'app/constants/heroletes/winterGridItems';
 import styles from './WinterGrid.module.scss';
+import { Link } from 'react-router-dom';
 
 export const WinterGrid = () => {
   return (
@@ -14,13 +15,15 @@ export const WinterGrid = () => {
         {React.Children.toArray(
           winterGridItems.map(item => (
             <Grid item lg={4} md={4} sm={6} xs={12} className={styles.winterGrid__item}>
-              <div className={styles.winterGrid__itemImg}>
-                <img src={item.image} alt="" />
-                <div className={styles.winterGrid__itemImgText}>
-                  <Typography variant="h6">{item.name}</Typography>
-                  <Typography>{item.sport}</Typography>
+              <Link to={`/heroletes/marketplace?athlete=${item.name}`}>
+                <div className={styles.winterGrid__itemImg}>
+                  <img src={item.image} alt="" />
+                  <div className={styles.winterGrid__itemImgText}>
+                    <Typography variant="h6">{item.name}</Typography>
+                    <Typography>{item.sport}</Typography>
+                  </div>
                 </div>
-              </div>
+              </Link>
             </Grid>
           ))
         )}

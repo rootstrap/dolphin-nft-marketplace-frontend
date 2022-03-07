9af37a9e68103ba0fd0359b79cd87aeefdfd77ee
Alejo Boga <alejoboga20@gmail.com>
2022-03-07 17:24:10 -0300

    Addin home carousel items

---
+++ src/app/constants/common/homeCarouselItems.ts
@@ -0,0 +1,21 @@
+import HeroletesBanner from 'app/assets/homeCarousel/HeroletesBanner.png';
+import HofvmBanner from 'app/assets/homeCarousel/HofvmBanner.png';
+import CreaturesBanner from 'app/assets/homeCarousel/CreaturesBanner.png';
+
+export const homeCarouselItems = [
+  {
+    title: 'Heroletes',
+    text: 'Digital collectibles that bring together the worlds of comic books and groundbreaking athletes...',
+    img: HeroletesBanner,
+  },
+  {
+    title: 'Hall of Fame Village Media',
+    text: 'Playbooks brings to life the most memorable plays from the college and pro careers of football legends ...',
+    img: HofvmBanner,
+  },
+  {
+    title: 'Creatures Chronicles',
+    text: 'A multi-season graphic event, woven through the universe, built in the metaverse and ready for exploration by our community.',
+    img: CreaturesBanner,
+  },
+];

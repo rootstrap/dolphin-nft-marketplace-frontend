5c143f5cbf5029846b680627edbadaf2298a8109
Alejo Boga <alejoboga20@gmail.com>
2022-03-17 09:33:47 -0300

    Removing unused imports

--- src/app/pages/Profile/MyCollections/MyCollections.tsx
+++ src/app/pages/Profile/MyCollections/MyCollections.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Button, Typography } from '@material-ui/core';
-import { LockOutlined } from '@material-ui/icons';
+import { Typography } from '@material-ui/core';
 import { winterGridItems } from 'app/constants/heroletes/winterGridItems';
 import { MyCollectionItem } from './MyCollectionItem';
 import useTranslation from 'app/hooks/useTranslation';

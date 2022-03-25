b330d753d6944113b47a19c95450071551305e87
Alejo Boga <alejoboga20@gmail.com>
2022-03-25 15:19:25 -0300

    Adding edit profile button

--- src/app/pages/Profile/Main/Main.tsx
+++ src/app/pages/Profile/Main/Main.tsx
@@ -1,19 +1,23 @@
+import { useContext } from 'react';
 import { Typography } from '@material-ui/core';
 import { ReactComponent as Twitter } from 'app/assets/Twitter.svg';
 import { ReactComponent as Discord } from 'app/assets/Discord.svg';
-import { useAppSelector } from 'app/hooks/reduxHooks';
+import { EditOutlined } from '@material-ui/icons';
+import { useResponsive } from 'app/hooks/useResponsive';
+import { ProfileContext } from '../ProfilePage';
 import styles from './Main.module.scss';
 
 export const Main = () => {
-  const { avatarImg, firstName, lastName, createdAt, email, twitterUrl, discordUrl } = useAppSelector(
-    state => state.user.user
-  );
+  const { handleOpenEditProfile, user } = useContext(ProfileContext);
+  const { isMobileView } = useResponsive();
+  const { avatarImg, firstName, lastName, createdAt, email, twitterUrl, discordUrl } = user;
 
   return (
     <div className={styles.main}>
       <div className={styles.main__img}>
         <img src={avatarImg} alt="avatar" />
       </div>
+
       <div className={styles.main__text}>
         <Typography variant="h4" component="div">
           {firstName} {lastName}
@@ -42,6 +46,17 @@ export const Main = () => {
           </a>
         </div>
       </div>
+
+      {!isMobileView && (
+        <div className={styles.main__edit}>
+          <div className={styles.main__editContainer}>
+            <Typography variant="subtitle1" onClick={handleOpenEditProfile}>
+              Edit profile
+            </Typography>
+            <EditOutlined />
+          </div>
+        </div>
+      )}
     </div>
   );
 };

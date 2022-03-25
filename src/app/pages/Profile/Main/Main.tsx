884a80350b301c58c7c9062cce5e654e648d714f
Alejo Boga <alejoboga20@gmail.com>
2022-03-25 10:20:12 -0300

    Destructuring user

--- src/app/pages/Profile/Main/Main.tsx
+++ src/app/pages/Profile/Main/Main.tsx
@@ -5,37 +5,39 @@ import { useAppSelector } from 'app/hooks/reduxHooks';
 import styles from './Main.module.scss';
 
 export const Main = () => {
-  const { user } = useAppSelector(state => state.user);
+  const { avatarImg, firstName, lastName, createdAt, email, twitterUrl, discordUrl } = useAppSelector(
+    state => state.user.user
+  );
 
   return (
     <div className={styles.main}>
       <div className={styles.main__img}>
-        <img src={user.avatarImg} alt="avatar" />
+        <img src={avatarImg} alt="avatar" />
       </div>
       <div className={styles.main__text}>
         <Typography variant="h4" component="div">
-          {user.firstName} {user.lastName}
+          {firstName} {lastName}
         </Typography>
       </div>
 
       <div className={styles.main__text}>
         <Typography variant="subtitle1" component="div">
-          Member since: {new Date(Date.parse(user.createdAt)).toDateString()}
+          Member since: {new Date(Date.parse(createdAt)).toDateString()}
         </Typography>
       </div>
 
       <div className={styles.main__text}>
         <Typography variant="subtitle1" component="div">
-          {user.email}
+          {email}
         </Typography>
       </div>
 
       <div className={styles.main__social}>
         <div className={styles.main__socialContainer}>
-          <a href={user.twitterUrl} target="_blank">
+          <a href={twitterUrl} target="_blank">
             <Twitter />
           </a>
-          <a href={user.discordUrl} target="_blank">
+          <a href={discordUrl} target="_blank">
             <Discord />
           </a>
         </div>
